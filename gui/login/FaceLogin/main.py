d13b23d399e629044483bc42a42a28996a5ad4a3
lechithinh <lechithinh.developer@gmail.com>
2022-12-20 14:50:58 +0700

    Init Face login

--- gui/login/FaceLogin/main.py
+++ gui/login/FaceLogin/main.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 
-from tkinter import Frame, Canvas, Entry, Text, Button, PhotoImage, messagebox
+from tkinter import Frame, Canvas,Label, Entry, Text, Button, PhotoImage, messagebox
 from controller import *
+from PIL import Image, ImageTk
+import cv2
+import imutils
 
 OUTPUT_PATH = Path(__file__).parent
 ASSETS_PATH = OUTPUT_PATH / Path("./assets")
@@ -22,29 +25,36 @@ class LoginByFace(Frame):
 
         self.configure(bg="#FFFFFF")
 
-        #cavans for text 
+        #Background
         self.canvas = Canvas(
             self,
             bg="#FFFFFF",
-            height=432,
-            width=797,
+            height=506,
+            width=543,
             bd=0,
             highlightthickness=0,
             relief="ridge",
         )
-
         self.canvas.place(x=0, y=0)
         
-        self.button_image_1 = PhotoImage(file=relative_to_assets("button_1.png"))
+
+        
+        self.label = Label(self, width=450, height=350, bg="#FFFFFF")
+        self.label.frame_num = 0
+        self.label.grid(row=3, column=0)
+        global video 
+        video = None
+        #LOGIN BUTTON
+        self.button_image_1 = PhotoImage(file=relative_to_assets("button_2.png"))
         button_1 = Button(
             self,
             image=self.button_image_1,
             borderwidth=0,
             highlightthickness=0,
-            command=self.checkface,
+            command=self.video_stream,
             relief="flat",
         )
-        button_1.place(x=150.0, y=382.0, width=190.0, height=48.0)
+        button_1.place(x=10.0, y=437.0, width=190.0, height=48.0)
         
         self.canvas.create_rectangle(
             56.0, 197.0, 169.0, 199.0, fill="#FFFFFF", outline=""
@@ -53,9 +63,48 @@ class LoginByFace(Frame):
         self.canvas.create_rectangle(
             418.0, 197.0, 531.0, 199.0, fill="#FFFFFF", outline=""
         )
+        
+        self.button_image_2 = PhotoImage(file=relative_to_assets("button_3.png"))
+        button_2 = Button(
+            self,
+            image=self.button_image_2,
+            borderwidth=0,
+            highlightthickness=0,
+            command=self.end_capture,
+            relief="flat",
+        )
+        button_2.place(x=250.0, y=437.0, width=190.0, height=48.0)
+        
 
-    #functions here to check face
-    def checkface(self):
-        print("correct")
+
+    def video_stream(self):
+        global video
+        video = cv2.VideoCapture(0)
+        self.start_capture()
+    
+    def start_capture(self):
+        global video
+        ret, frame = video.read()
+        if ret:
+            frame = imutils.resize(frame, width = 640)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(frame)
+            image = ImageTk.PhotoImage(image=img)
+            self.label.configure(image=image)
+            self.label.image = image
+            self.label.imgtk = image
+            self.label.after(10,self.start_capture)
+        else:
+            video.release()
+            
+
+    
+    def end_capture(self):
+        global video
+        imagetk = self.label.imgtk
+        imgpil = ImageTk.getimage( imagetk )
+        print(imgpil)
+        self.label.place_forget()
+        video.release()
 
 
